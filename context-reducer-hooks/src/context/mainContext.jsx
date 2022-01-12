1cf6bdc6664b151ef68c2eb2d8f168b6b4ec6946
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-12 13:38:06 +0200

    📂 Create main context

---
+++ context-reducer-hooks/src/context/mainContext.jsx
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+//Creating the context for future use with the help of React's hook
+// Will be used by us in the future to create a component that will bring out with the context what we want
+const mainContext = createContext();
+
+export default mainContext;
