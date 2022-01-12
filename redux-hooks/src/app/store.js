fb1f0fcf471feb1301bdb70799097216d2dd5e2f
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-12 18:13:38 +0200

    👩‍💻 Provide redux-thunk middleware to the store

--- redux-hooks/src/app/store.js
+++ redux-hooks/src/app/store.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import blogsSlice from '../reducers/blogsReducer';
 import advicesSlice from '../reducers/adviceSlice';
+import thunkMiddleWare from 'redux-thunk';
 
 export const store = configureStore({
   reducer: {
     blogs: blogsSlice,
     advices: advicesSlice,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(thunkMiddleWare),
 });
 
 export default store;
