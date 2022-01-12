02e43680bfb14b68e53a00665e14e3e0f4108c46
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-12 15:55:17 +0200

    👩‍💻 Use mapDispatchToProps

--- redux-connect/src/components/App.jsx
+++ redux-connect/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-function App({ blogs }) {
+function App({ blogs, addLike }) {
   return (
     <div className='App'>
       <h1>Hello Redux connect</h1>
@@ -23,4 +23,11 @@ const mapStateToProps = state => {
   return { blogs: state };
 };
 
-export default connect(mapStateToProps)(App);
+// Get functions that activate the reducer
+//A function that accepts a dispatch as an argument and returns an object that the key will be the name of the props
+//and the value will be a function that dispatches certain actions
+const mapDispatchToProps = dispatch => {
+  return { addLike: url => dispatch({ type: 'LIKE_INCREMENT', payload: url }) };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
