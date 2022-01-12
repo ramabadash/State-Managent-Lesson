9a91ff0c2815b8cc6d0ccfb6aaba9a3b0272e9b4
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-12 13:43:08 +0200

    📂 Create main provider - no reducer yet

---
+++ context-reducer-hooks/src/context/mainProvider.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import mainContext from './main-context';
+import blogsList from '../data/blogsList';
+
+// A component that contains functionality and state and exports them to all children by using context
+function mainProvider({ children }) {
+  return (
+    <mainContext.Provider
+      value={{
+        blogsList,
+      }}
+    >
+      {children}
+    </mainContext.Provider>
+  );
+}
+
+export default mainProvider;
