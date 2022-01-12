ceaa95d3b29abc442c32457e46f1c8730b07a4e7
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-12 15:18:27 +0200

    👩‍💻 Create Basic reducer

---
+++ redux-connect/src/reducers/mainReducer.js
@@ -0,0 +1,26 @@
+import blogsList from '../data/blogsList';
+
+// Switch case function
+// Get state and action and by the actions type decide what to do
+// You can send why more information - it is customary to send in action within payload what you want
+
+const mainReducer = (state = blogsList, { type, payload }) => {
+  switch (type) {
+    case 'INITIAL_DATA': {
+      return [...payload.data];
+    }
+    case 'LIKE_INCREMENT': {
+      const newState = [...state]; // Copy the current state
+      for (const blog of newState) {
+        if (blog.url === payload.url) {
+          blog.likes += 1;
+        }
+      }
+      return [...newState];
+    }
+    default:
+      return state;
+  }
+};
+
+export default mainReducer;
