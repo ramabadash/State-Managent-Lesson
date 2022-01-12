b61d0e342f8b0633676d7064a4bfcafe06fab211
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-12 15:42:19 +0200

    👩‍💻 Provide the store to the App

--- redux-connect/src/app/store.js
+++ redux-connect/src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import rootReducer from './reducers/mainReducer';
+import mainReducer from '../reducers/mainReducer';
 
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({ reducer: mainReducer });
 
 export default store;
