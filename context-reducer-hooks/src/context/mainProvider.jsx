5ca05ce96806adddb57bba2321f057086da5541a
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-12 14:27:09 +0200

    👩‍💻 Use useReducer to provide the state and dispatch

--- context-reducer-hooks/src/context/mainProvider.jsx
+++ context-reducer-hooks/src/context/mainProvider.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import mainContext from './mainContext';
+// Date
 import blogsList from '../data/blogsList';
+//
+import mainReducer from '../reducers/mainReducer';
 
 // A component that contains functionality and state and exports them to all children by using context
 function MainProvider({ children }) {
+  // hook that receives the reducer and the initial state
+  // Returns the state and a function that is the one that knows how to activate the reducer - it is commonly called dispatch
+  const [blogs, dispatch] = useReducer(mainReducer, [...blogsList]);
+
   return (
     <mainContext.Provider
       value={{
-        blogsList,
+        blogs,
+        dispatch,
       }}
     >
       {children}
