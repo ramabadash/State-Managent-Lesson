6b80ecf9c110d776db20533bdd111b96220c47ce
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-12 17:18:50 +0200

    👩‍💻 Create advicesSlice - initial state, actions and reducer

---
+++ redux-hooks/src/reducers/adviceSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  advices: [],
+};
+
+const advicesSlice = createSlice({
+  name: 'advices',
+  initialState,
+  reducers: {
+    initialData: (state, { payload }) => {
+      return [...payload.data];
+    },
+    addAdvice: (state, { payload }) => {
+      return [...state, payload.advice];
+    },
+  },
+});
+
+// Export the actions
+export const { initialData, addAdvice } = advicesSlice.actions;
+
+// Export the reducer
+export default advicesSlice.reducer;
