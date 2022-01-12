c2d9ca183c92cf24c8836d757d9e62306f0c9a21
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-12 18:15:28 +0200

    👩‍💻 Add API request and dispatch on click

--- redux-hooks/src/components/AdvicesList.jsx
+++ redux-hooks/src/components/AdvicesList.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { addAdvice } from '../reducers/adviceSlice';
 
 function AdvicesList() {
   /***** STATES *****/
   const advices = useSelector(({ advices }) => advices);
 
+  /***** FUNCTIONS *****/
+  // Dispatch
+  const dispatch = useDispatch();
+
+  // Async Fetch weather request
+  const fetchAdvice = () => {
+    return dispatch => {
+      axios
+        .get(`https://api.adviceslip.com/advice`)
+        .then(({ data }) => {
+          dispatch(addAdvice({ advice: data.slip.advice }));
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    };
+  };
   return (
     <div>
       <h1>Good to know:</h1>
+      <button onClick={() => dispatch(fetchAdvice())}>
+        Give me more advice 💡
+      </button>
       <ul>
         {advices.length ? (
           advices.map(advice => <li key={advice}>{advice}</li>)
